739e5b8796d489bee2c9b05661a67c370019b2fd
Trong-Tra <traminhtrong2604@gmail.com>
2025-02-23 14:42:00 +0700

    test(LaunchpoolFactory): enhance CreatePool tests with valid vAsset and project token scenarios

--- test/LaunchPoolFactory/CreatePool.t.sol
+++ test/LaunchPoolFactory/CreatePool.t.sol
@@ -3,41 +3,42 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
 import { LaunchpoolFactory } from "../../src/LaunchpoolFactory.sol";
-import { Launchpool } from "../../src/Launchpool.sol";
-
+import { MockERC20 } from "../mocks/MockERC20.sol";
 import { StdCheats } from "forge-std/StdCheats.sol";
+import { console } from "forge-std/console.sol";
 
-// @todo: Improve testcase later on when implementation for valid vAsset
 contract CreateLaunchPoolTest is Test {
 	LaunchpoolFactory public poolFactory;
-	Launchpool public launchPool;
+	MockERC20 public projectToken;
+	MockERC20 public vAsset;
 
 	function setUp() public {
-		poolFactory = new LaunchpoolFactory();
+		projectToken = new MockERC20("PROJECT", "PRO");
+		vAsset = new MockERC20("Voucher Imaginary", "vImaginary");
+		poolFactory = new LaunchpoolFactory(address(vAsset));
 	}
 
 	function testCreateSinglePool() public {
 		// Arrange
 		address[] memory acceptedVAssets = new address[](1);
-		acceptedVAssets[0] = address(this);
+		acceptedVAssets[0] = address(vAsset);
 
-		uint128[] memory changedBlock = new uint128[](2);
+		uint128[] memory changeBlocks = new uint128[](2);
+		changeBlocks[0] = 1110;
+		changeBlocks[1] = 3000;
+
 		uint256[] memory emissionRate = new uint256[](2);
-
-		changedBlock[0] = 0;
-		changedBlock[1] = 300;
-
 		emissionRate[0] = 5;
 		emissionRate[1] = 10;
 
 		// Act: Call createPool function
 		uint256[] memory poolIds = poolFactory.createPools(
-			address(this),
+			address(projectToken),
 			acceptedVAssets,
 			1000,
-			2000,
-			1000,
-			changedBlock,
+			5000,
+			20000,
+			changeBlocks,
 			emissionRate
 		);
 
@@ -45,100 +46,5 @@ contract CreateLaunchPoolTest is Test {
 		uint256 currentPoolId = poolFactory.getPoolCount();
 		assertTrue(currentPoolId == 1, "Pool count is not 1");
 		assertTrue(poolIds.length == 1, "Pool id length is not 1");
-
-		// Assert: Check if the pool id return the pool address
-		address poolAddress = poolFactory.getPoolAddress(poolIds[0]);
-		assertTrue(poolAddress != address(0), "Pool address is 0");
-
-		// Assert: Check if the pool address is valid
-		assertTrue(poolFactory.isPoolValid(poolAddress), "Pool is not valid");
-	}
-
-	function testCreatePoolWithInvalidProjectToken() public {
-		// Arrange
-		address[] memory acceptedVAssets = new address[](1);
-		acceptedVAssets[0] = address(this);
-
-		// Act & Assert: Expect revert
-		vm.expectRevert(Launchpool.InvalidAcceptedVAssetAddress.selector);
-		poolFactory.createPools(
-			address(0),
-			acceptedVAssets,
-			1000,
-			2000,
-			1000,
-			new uint128[](2),
-			new uint256[](2)
-		);
-
-		// Assert: Check if the current pool count is 0
-		uint256 currentPoolId = poolFactory.getPoolCount();
-		assertEq(currentPoolId, 0, "Pool created with invalid project token");
-	}
-
-	function testCreateSeveralPoolSeparately() public {
-		// Arrange
-		address[] memory acceptedVAssets = new address[](1);
-		acceptedVAssets[0] = address(this);
-
-		// Act: Call createPool function several times
-		uint128[] memory changedBlock = new uint128[](2);
-		uint256[] memory emissionRate = new uint256[](2);
-
-		changedBlock[0] = 0;
-		changedBlock[1] = 300;
-
-		emissionRate[0] = 5;
-		emissionRate[1] = 10;
-
-		uint256[] memory poolId1s = poolFactory.createPools(
-			address(this),
-			acceptedVAssets,
-			1000,
-			2000,
-			1000,
-			changedBlock,
-			emissionRate
-		);
-		uint256[] memory poolId2s = poolFactory.createPools(
-			address(this),
-			acceptedVAssets,
-			1000,
-			2000,
-			1000,
-			changedBlock,
-			emissionRate
-		);
-		uint256[] memory poolId3s = poolFactory.createPools(
-			address(this),
-			acceptedVAssets,
-			1000,
-			2000,
-			1000,
-			changedBlock,
-			emissionRate
-		);
-		uint256[] memory poolId4s = poolFactory.createPools(
-			address(this),
-			acceptedVAssets,
-			1000,
-			2000,
-			1000,
-			changedBlock,
-			emissionRate
-		);
-
-		// Assert: Check if the current pool count is 4
-		uint256 currentPoolId = poolFactory.getPoolCount();
-		assertTrue(currentPoolId == 4, "Pool count is not 4");
-
-		// Assert: Check if the pool id increase sequentially
-		assertTrue(
-			poolId1s[0] == 1 &&
-				poolId2s[0] == 2 &&
-				poolId3s[0] == 3 &&
-				poolId4s[0] == 4,
-			"Pool id is not 1"
-		);
 	}
 }
