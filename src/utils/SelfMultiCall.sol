a24971e9596b71d0a2f793c8267d7bc12201ac2a
ducmint864 <nddminh2021@gmail.com>
2025-03-07 10:08:49 +0700

    feat(multicall): implement mechanism to determine original tx sender in SelfMulticall
    * note: moved SelfMulticall.sol to src/utils

--- src/utils/SelfMultiCall.sol
+++ src/utils/SelfMultiCall.sol
@@ -3,6 +3,9 @@
 pragma solidity ^0.8.26;
 
 contract SelfMultiCall {
+	bytes32 public constant MULTICALL_SENDER_SLOT =
+		keccak256("SelfMultiCall.ORIGINAL_SENDER_SLOT");
+
 	error MultiCallFailed(
 		uint256 callIndex,
 		bytes callPayload,
@@ -24,11 +27,14 @@ contract SelfMultiCall {
 
 		allReturnData = new bytes[](len);
 
+		_setMultiCallSender(msg.sender);
+
 		for (uint256 i; i < len; ) {
 			(bool success, bytes memory returnData) = address(this).call(
 				callPayloadBatch[i]
 			);
 			if (!success) {
+				_setMultiCallSender(address(0));
 				revert MultiCallFailed(i, callPayloadBatch[i], returnData);
 			}
 			allReturnData[i] = returnData;
@@ -38,36 +44,26 @@ contract SelfMultiCall {
 			}
 		}
 
+		_setMultiCallSender(address(0));
 		return allReturnData;
 	}
 
-	/**
-	 * @notice Executes multiple view functions in a single call
-	 * @param callPayloadBatch Array of encoded function calls
-	 * @return Array of return values
-	 */
-	function selfMultiCallStatic(
-		bytes[] calldata callPayloadBatch
-	) external view returns (bytes[] memory) {
-		uint256 len = callPayloadBatch.length;
-		if (len == 0) return new bytes[](0);
-
-		bytes[] memory allReturnData = new bytes[](len);
-
-		for (uint256 i; i < len; ) {
-			(bool success, bytes memory returnData) = address(this).staticcall(
-				callPayloadBatch[i]
-			);
-			if (!success) {
-				revert MultiCallFailed(i, callPayloadBatch[i], returnData);
-			}
-			allReturnData[i] = returnData;
-
-			unchecked {
-				++i;
-			}
+	function _setMultiCallSender(address sender) internal {
+		bytes32 position = MULTICALL_SENDER_SLOT;
+		assembly {
+			sstore(position, sender)
 		}
+	}
 
-		return allReturnData;
+	/**
+	 * @notice Context-preserving version of msg.sender
+	 * for use in selfMultiCall
+	 */
+	function _getMultiCallSender() internal view returns (address sender) {
+		bytes32 position = MULTICALL_SENDER_SLOT;
+		assembly {
+			sender := sload(position)
+		}
+		return sender;
 	}
 }
