3f729ab1bb8dd80f20329a74377f96623808e2f5
ducmint864 <nddminh2021@gmail.com>
2025-03-07 10:19:59 +0700

    refactor(IProjectHub): integrate ProjectLibrary and LaunchpoolLibrary

--- src/interfaces/IProjectHub.sol
+++ src/interfaces/IProjectHub.sol
@@ -2,57 +2,9 @@
 
 pragma solidity ^0.8.26;
 
+import { ProjectLibrary, LaunchpoolLibrary } from "@src/upgradeable/v1/ProjectHubUpgradeable.sol";
+
 interface IProjectHub {
-	// Type definitions
-	enum PoolType {
-		LAUNCHPOOL,
-		LAUNCHPAD,
-		VESTING,
-		FARMING
-	}
-
-	struct Project {
-		uint64 projectId;
-		address projectOwner;
-	}
-
-	struct Pool {
-		uint64 poolId;
-		PoolType poolType;
-		address poolAddress;
-		uint64 projectId;
-		bool isListed;
-	}
-
-	struct LaunchpoolCreationParams {
-		uint64 projectId;
-		address projectToken;
-		address vAsset;
-		uint128 startBlock;
-		uint128 endBlock;
-		uint256 maxVTokensPerStaker;
-		uint128[] changeBlocks;
-		uint256[] emissionRateChanges;
-		bool isListed;
-	}
-
-	// Events
-	event ProjectCreated(
-		uint64 indexed projectId,
-		address indexed projectOwner
-	);
-	event PoolCreated(
-		uint64 indexed projectId,
-		PoolType indexed poolType,
-		uint64 poolId,
-		address projectToken,
-		address indexed vAsset,
-		address poolAddress,
-		uint128 startBlock,
-		uint128 endBlock
-	);
-	event ProjectListingChanged(uint64 indexed projectId, bool isListed);
-
 	// Functions
 	function initialize(
 		address _initialOwner,
@@ -66,7 +18,7 @@ interface IProjectHub {
 	function unlistProject(uint64 _poolId) external;
 
 	function createLaunchpool(
-		LaunchpoolCreationParams memory params
+		LaunchpoolLibrary.LaunchpoolCreationParams memory params
 	) external returns (uint64);
 
 	function setAcceptedVAsset(address _vAsset, bool _isAccepted) external;
@@ -77,9 +29,13 @@ interface IProjectHub {
 	) external returns (bytes[] memory results);
 
 	// View Functions
-	function projects(uint64 projectId) external view returns (Project memory);
+	function projects(
+		uint64 projectId
+	) external view returns (ProjectLibrary.Project memory);
 
-	function pools(uint64 poolId) external view returns (Pool memory);
+	function pools(
+		uint64 poolId
+	) external view returns (LaunchpoolLibrary.Pool memory);
 
 	function acceptedVAssets(address _vAsset) external view returns (bool);
 
