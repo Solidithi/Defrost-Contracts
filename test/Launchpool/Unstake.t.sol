a140c59937f5805a519238facda107309ee216d6
Trong-Tra <traminhtrong2604@gmail.com>
2025-03-09 20:50:54 +0700

    fix: update import path for Launchpool contract

--- test/Launchpool/Unstake.t.sol
+++ test/Launchpool/Unstake.t.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.24;
 
 import "forge-std/Test.sol";
 import { MockLaunchpool } from "../mocks/MockLaunchpool.sol";
-import { Launchpool } from "../../src/Launchpool.sol";
+import { Launchpool } from "@src/non-upgradeable/Launchpool.sol";
 import { MockERC20 } from "../mocks/MockERC20.sol";
 
 import { MockXCMOracle } from "../mocks/MockXCMOracle.sol";
