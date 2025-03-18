2ff1626fa6123e5c9631759b2fbc8e2457c1a42f
Trong-Tra <traminhtrong2604@gmail.com>
2025-03-18 20:50:28 +0700

    feat(oracle): add PoolInfo and RateInfo structs to IXCMOracle interface

--- src/interfaces/IXCMOracle.sol
+++ src/interfaces/IXCMOracle.sol
@@ -2,6 +2,25 @@
 pragma solidity ^0.8.26;
 
 interface IXCMOracle {
+	struct PoolInfo {
+		uint256 assetAmount;
+		uint256 vAssetAmount;
+	}
+	struct RateInfo {
+		uint8 mintRate;
+		uint8 redeemRate;
+	}
+
+	function getCurrencyIdByAssetAddress(
+		address _assetAddress
+	) external view returns (bytes2);
+
+	function tokenPool(
+		bytes2 _currencyId
+	) external view returns (PoolInfo memory);
+
+	function rateInfo() external view returns (RateInfo memory);
+
 	// _assetAddress: Asset address, e.g. DOT, KSM
 	// _assetAmount: Input asset amount, get vAsset amount
 	// _vAssetAmount: Input vAsset amount, get asset amount
@@ -14,9 +33,4 @@ interface IXCMOracle {
 		address _assetAddress,
 		uint256 _vAssetAmount
 	) external view returns (uint256);
-
-	function rateInfo()
-		external
-		view
-		returns (uint8 mintRate, uint8 redeemRate);
 }
