a54284abd8015cbb1c92a60d2ceac4bb76790fef
ducmint864 <nddminh2021@gmail.com>
2025-03-27 18:25:42 +0700

    feat(SetupStakers): add random salt generation for more 'unique' staker addresses

--- test/testutils/SetupStakers.sol
+++ test/testutils/SetupStakers.sol
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.26;
+
 import "forge-std/Test.sol";
 import { MockERC20 } from "@src/mocks/MockERC20.sol";
 
@@ -9,10 +13,23 @@ contract SetupStakers is Test {
 		address launchpoolAddr,
 		address vAsset
 	) public returns (address[] memory) {
+		// Generate a random salt for the stakers
+		uint256 salt = uint256(
+			keccak256(
+				abi.encodePacked(
+					block.timestamp,
+					block.prevrandao,
+					stakerCount,
+					msg.sender,
+					tx.origin,
+					block.coinbase
+				)
+			)
+		);
 		address[] memory stakers = new address[](stakerCount);
 		for (uint i; i < stakerCount; i++) {
 			address newStaker = makeAddr(
-				string(abi.encodePacked("staker ", i))
+				string(abi.encodePacked("staker ", i, salt))
 			);
 			stakers[i] = newStaker;
 			MockERC20(vAsset).freeMintTo(newStaker, 1e55);
